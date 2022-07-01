811e835da72ba800c83b3ee8e1829ad4c83b270d
ENGINEERS IT <abedur@gmail.com>
2022-06-30 22:25:05 -0500

    Lab 7 added

---
+++ lab7/lab2.js
@@ -0,0 +1,26 @@
+/*
+With Arrow function
+let group = {
+    title: "Our Group",
+    students: ["John", "Pete", "Alice"],
+    showList: function () {
+        this.students.forEach((student)=> {
+            console.log(this.title + ": " + student
+            );
+        });
+    }
+};
+group.showList();
+*/
+// With bind
+let group = {
+    title: "Our Group",
+    students: ["John", "Pete", "Alice"],
+    showList: function () {
+        this.students.forEach(function (student) {
+            console.log(this.title + ": " + student
+            );
+        }.bind(this));
+    }
+};
+group.showList();
